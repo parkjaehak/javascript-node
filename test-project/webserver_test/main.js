887ce53f5c970eb29f01aa896bffc8c8b938e082
parkjaehak <atom8426@gmail.com>
2025-06-07 07:09:26 +0000

    fix: open in browser 문제 해결

--- test-project/webserver_test/main.js
+++ test-project/webserver_test/main.js
@@ -1,18 +1,18 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
+ 
 var app = http.createServer(function(request,response){
-var _url = request.url;
-var queryData = url.parse(_url, true).query;
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
     var title = queryData.id;
-if(_url == '/'){
+    if(_url == '/'){
       title = 'Welcome';
-}
-if(_url == '/favicon.ico'){
-return response.writeHead(404);
-}
-response.writeHead(200);
+    }
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
     var template = `
     <!doctype html>
     <html>
@@ -25,7 +25,8 @@ response.writeHead(200);
       <ul>
         <li><a href="/?id=HTML">HTML</a></li>
         <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
+        <li><a href="/?id=Java
+        Script">JavaScript</a></li>
       </ul>
       <h2>${title}</h2>
       <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
@@ -36,6 +37,6 @@ response.writeHead(200);
     </html>
     `;
     response.end(template);
-
+ 
 });
 app.listen(3000);
